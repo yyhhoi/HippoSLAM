2b5ff7ab6958a09bcfc2e9669f3623f7c7b5ea79
yyhhoi <yyhhoi@gmail.com>
2024-02-08 16:30:19 +0100

    clean up

--- controllers/hipposlam/AWAC_ExampleScript.py
+++ controllers/hipposlam/AWAC_ExampleScript.py
@@ -6,34 +6,31 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from hipposlam.Networks import ActorModel, QCriticModel, MLP
-from hipposlam.Replay import Replayer, ReplayMemoryCat
+from hipposlam.Replay import ReplayMemoryAWAC
 from hipposlam.ReinforcementLearning import AWAC
 
-
+# Paths and parameters
 env = gym.make("CartPole-v1")
-
 obs_dim = 4
 act_dim = 2
-
 gamma = 0.9
 lam = 1.0
+
+
+# Networks
 critic = QCriticModel(obs_dim, act_dim)
 critic_target = QCriticModel(obs_dim, act_dim)
 actor = ActorModel(obs_dim, act_dim, logit=True)
 
-# critic = MLP(4, 2,
-#            num_neurons=[128,128],
-#            out_act='ReLU')
-# critic_target = MLP(4, 2,
-#                   num_neurons=[128,128],
-#                   out_act='ReLU')
-# actor = MLP(4, 2, num_neurons=[128,64])
-
+# Replay Memory
 batch_size = 1024
-memory = ReplayMemoryCat(max_size=500000)
+max_buffer_size = 500000
+memory = ReplayMemoryAWAC(max_size=max_buffer_size)
 datainds = np.cumsum([0, obs_dim, 1, obs_dim, 1, 1])
 memory.specify_data_tuple(s=(datainds[0], datainds[1]), a=(datainds[1], datainds[2]),
                           snext=(datainds[2], datainds[3]), r=(datainds[3], datainds[4]), end=(datainds[4], datainds[5]))
+
+# AWAC wrapper
 agent = AWAC(critic, critic_target, actor,
              lam=lam,
              gamma=gamma,
@@ -71,12 +68,6 @@ for n_epi in range(Niters):
         actor_loss = agent.update_actor(_s, _a)
         critic_losses.append(critic_loss.detach())
         actor_losses.append(actor_loss.detach())
-        # for _s, _a, _snext, _r, _end in memory.sample(batch_size):
-        #     critic_loss = agent.update_critic(_s, _a, _snext, _r, _end)
-        #     actor_loss = agent.update_actor(_s, _a)
-        #     critic_losses.append(critic_loss.detach())
-        #     actor_losses.append(actor_loss.detach())
-        #     break
 
 
 
